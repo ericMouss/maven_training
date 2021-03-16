439eb8ecc2eb40091eba0a179c739e0a63026707
eric Mouss <Manfoumbieric@gmail.com>
2021-03-17 00:47:33 +0100

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -2,19 +2,29 @@ package fr.lernejo;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.opentest4j.AssertionFailedError;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
     private final Sample sample = new Sample();
-    @Test
-    void test1(){
-        int result = sample.op(Sample.Operation.ADD, 7, 3);
+    @ParameterizedTest
+    @CsvSource({
+        "3, 7, 10",
+        "0, 3, 3"
+    })
+    void test1(int a, int b, int expectedR){
+        int result = sample.op(Sample.Operation.ADD,a ,b);
 
-        Assertions.assertThat(result).isEqualTo(12);
-        if(result != 12){
-            throw new AssertionFailedError(result + " != 12");
-        }
+        Assertions.assertThat(result).isEqualTo(expectedR);
     }
+    /*
+    @Test
+    void op_throw_if_operation_is_null(){
+        Assertions.assertThatExceptionOfType(NullPointerException.class)
+            .isThrownBy(()-> sample.op(null, 1, 3));
+    }*/
+
 }
